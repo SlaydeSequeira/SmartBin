ffdd7b0472c1e8f6e45ba1b72fe6db02eab2b468
Slayde <slaydes13@gmail.com>
2023-07-13 22:53:03 +0530

    unique id generation

--- app/src/main/java/com/example/smartbin/LoginPage.java
+++ app/src/main/java/com/example/smartbin/LoginPage.java
@@ -3,15 +3,29 @@ package com.example.smartbin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Objects;
+import java.util.Random;
 
 public class LoginPage extends AppCompatActivity {
 
+    int uniqueid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
         Objects.requireNonNull(getSupportActionBar()).hide();
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random= new Random();
+                int randomNumber = random.nextInt(1000000);
+                Toast.makeText(LoginPage.this, uniqueid, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/smartbin/MainActivity.java
+++ app/src/main/java/com/example/smartbin/MainActivity.java
@@ -3,17 +3,22 @@ package com.example.smartbin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button= findViewById(R.id.button);
+        Button button = findViewById(R.id.button);
+        int tint = Color.parseColor("#081DA8");
+        PorterDuffColorFilter colorFilter = new PorterDuffColorFilter(tint, PorterDuff.Mode.SRC_IN);
+        button.getBackground().setColorFilter(colorFilter);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
